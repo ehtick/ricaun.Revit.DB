e279b9aa269d768e397d0879656b393a6a30ed0e
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-10-06 22:53:03 -0300

    Update to Alpha

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,8 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-class Build : NukeBuild, IPublishPack, IBuildPackages, ITestLocal
+class Build : NukeBuild, IPublishPack, ICompileExample, ITestLocal //, IBuildPackages
 {
+    string IHazExample.Name => "ricaun.Revit.DB.*";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
 }
