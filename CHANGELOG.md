5a18a952b748382a2209b721e90123d9f7d72174
Luiz Henrique Cassettari <ricaun@users.noreply.github.com>
2023-10-25 22:44:02 -0300

    Update Command

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 ## [0.3.0] / 2023-10-24
 ### Shape
 - Rename `indexes` to `indices`.
+- [] GetColor, GetMaterial and GetTriangleColors
 
 ## [0.2.0] / 2023-10-01 - 2023-10-21
 ### Shape
--- ricaun.Revit.DB.Shape/Revit/Commands/TessellatedShapeTwoBoxCommand.cs
+++ ricaun.Revit.DB.Shape/Revit/Commands/TessellatedShapeTwoBoxCommand.cs
@@ -25,9 +25,15 @@ namespace ricaun.Revit.DB.Shape.Revit.Commands
                 var materialGreen = MaterialUtils.CreateMaterialGreen(document);
                 var materialBlue = MaterialUtils.CreateMaterialBlue(document);
 
+                var materialWhite = MaterialUtils.CreateMaterialWhite(document);
+
+                var materialBlue2 = MaterialUtils.CreateMaterial(document, Colors.Index.Color_150);
+                var materialBlue3 = MaterialUtils.CreateMaterial(document, Colors.Index.Color_151);
+                var materialBlue4 = MaterialUtils.CreateMaterial(document, Colors.Index.Color_152);
+
                 var shape1 = ShapeCreator.CreateBox(XYZ.Zero, 1);
                 var shape2 = ShapeCreator.CreateBox(XYZ.Zero, 1)
-                    .CreateTransformed(Transform.CreateTranslation(10 * XYZ.BasisX));
+                    .CreateTransformed(Transform.CreateTranslation(2 * XYZ.BasisX).ScaleBasis(0.5));
 
                 var tessellatedShape = TessellatedShapeCreator.CreateMesh(
                     shape1.GetTriangleVertices().Concat(shape2.GetTriangleVertices()).ToArray(), materialIds: new[] { materialGreen.Id, materialGreen.Id, materialBlue.Id, materialBlue.Id });
@@ -35,10 +41,10 @@ namespace ricaun.Revit.DB.Shape.Revit.Commands
                 document.CreateDirectShape(tessellatedShape.GetGeometricalObjects());
 
                 var shape3 = ShapeCreator.CreateBox(XYZ.Zero, 1)
-                    .CreateTransformed(Transform.CreateTranslation(2 * XYZ.BasisX));
+                    .CreateTransformed(Transform.CreateTranslation(2.0 * XYZ.BasisX + 2 * XYZ.BasisY + 1e-6 * XYZ.BasisZ));
 
                 var tessellatedShape2 = TessellatedShapeCreator.CreateMesh(
-                    shape1.GetTriangleVertices().Concat(shape3.GetTriangleVertices()).ToArray(), materialIds: new[] { materialBlue.Id });
+                    shape1.GetTriangleVertices().Concat(shape3.GetTriangleVertices()).ToArray(), materialIds: new[] { materialBlue2.Id, materialBlue2.Id, materialBlue3.Id, materialBlue3.Id, materialBlue4.Id, materialBlue4.Id });
 
                 var meshShape = document.CreateDirectShape(tessellatedShape2.GetGeometricalObjects());
                 meshShape.Location.Move(2 * XYZ.BasisZ);
@@ -47,6 +53,13 @@ namespace ricaun.Revit.DB.Shape.Revit.Commands
                 if (mesh != null)
                 {
                     document.CreateDirectShape(TessellatedShapeCreator.CreateMesh(mesh.GetTriangleVertices().ToArray(), materialIds: new[] { materialGreen.Id, materialBlue.Id, materialRed.Id }).GetGeometricalObjects())
+                        .Location.Move(2 * XYZ.BasisZ);
+                }
+
+                var solid = meshShape.get_Geometry(new Options()).OfType<Solid>().FirstOrDefault();
+                if (solid != null)
+                {
+                    document.CreateDirectShape(TessellatedShapeCreator.CreateMesh(solid.GetTriangleVertices().ToArray(), materialIds: new[] { materialGreen.Id, materialBlue.Id, materialRed.Id, materialWhite.Id }).GetGeometricalObjects())
                         .Location.Move(2 * XYZ.BasisZ);
                 }
 
