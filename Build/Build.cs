0783b113b29a8a7275bdb294d09203af699f3a00
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-07-22 14:18:11 -0300

    Update `Build` to support tests with old versions

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,7 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-class Build : NukeBuild, IPublishPack, IPrePack, ICompileExample, ITestLocal
+class Build : NukeBuild, IPublishPack, IPrePack, ICompileExample, ITest
 {
     string IHazExample.Name => "ricaun.Revit.DB.*";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
