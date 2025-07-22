c204178dfda2d8f59e28e98d508ff7c57924d00a
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-07-22 13:58:43 -0300

    Update `ITestLocal` to local

--- Build/Build.cs
+++ Build/Build.cs
@@ -3,7 +3,7 @@ using Nuke.Common.Execution;
 using ricaun.Nuke;
 using ricaun.Nuke.Components;
 
-class Build : NukeBuild, IPublishPack, IPrePack, ICompileExample, ITest
+class Build : NukeBuild, IPublishPack, IPrePack, ICompileExample, ITestLocal
 {
     string IHazExample.Name => "ricaun.Revit.DB.*";
     public static int Main() => Execute<Build>(x => x.From<IPublishPack>().Build);
