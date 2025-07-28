3ffde8f271aeb6877816320a9de08452af8eac6b
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-07-28 14:37:35 -0300

    Add `RevitFilterCategoryExtension` tests for `FilterBuiltInCategoryExtension`

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -18,6 +18,7 @@ and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.
 - Add `ricaun.Revit.DB.Tests` to test `Select` extension for `Element` and `ElementType`.
 - Add `RevitFilterExtensionTest` tests for `ElementFilterExtension`.
 - Add `RevitElementIdExtensionTest` and `RevitElementIdToElementExtensionTest` tests for `ElementIdExtension`.
+- Add `RevitFilterCategoryExtension` tests for `FilterBuiltInCategoryExtension`.
 ### Shapes
 - Add `CreateSwept` to `ShapeCreator` to create a swept solid.
 ### Generator
---
+++ ricaun.Revit.DB.Tests/RevitFilterCategoryExtension.cs
@@ -0,0 +1,47 @@
+﻿using Autodesk.Revit.DB;
+using NUnit.Framework;
+
+namespace ricaun.Revit.DB.Tests
+{
+    public class RevitFilterCategoryExtension
+    {
+        [Test]
+        public void FilterBuiltInCategory()
+        {
+            var value = BuiltInCategory.OST_ProjectInformation;
+            var filter = value.Filter();
+            var filterInverted = value.Filter(true);
+
+            Assert.IsNotNull(filter);
+            Assert.IsNotNull(filterInverted);
+
+            Assert.IsInstanceOf<ElementCategoryFilter>(filter);
+            Assert.IsInstanceOf<ElementCategoryFilter>(filterInverted);
+
+            Assert.False(filter.Inverted);
+            Assert.True(filterInverted.Inverted);
+        }
+
+        [Test]
+        public void FilterBuiltInCategories()
+        {
+            var values = new[] {
+                BuiltInCategory.OST_Floors,
+                BuiltInCategory.OST_Walls
+            };
+
+            var filter = values.Filter();
+            var filterInverted = values.Filter(true);
+
+            Assert.IsNotNull(filter);
+            Assert.IsNotNull(filterInverted);
+
+            Assert.IsInstanceOf<ElementMulticategoryFilter>(filter);
+            Assert.IsInstanceOf<ElementMulticategoryFilter>(filterInverted);
+
+            Assert.False(filter.Inverted);
+            Assert.True(filterInverted.Inverted);
+        }
+
+    }
+}
