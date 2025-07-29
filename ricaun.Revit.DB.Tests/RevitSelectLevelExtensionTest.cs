04508f022b111de5316c78efb7d42aefc2c2e23d
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-07-29 15:03:11 -0300

    Update test `SelectLevel`

--- ricaun.Revit.DB.Tests/RevitSelectLevelExtensionTest.cs
+++ ricaun.Revit.DB.Tests/RevitSelectLevelExtensionTest.cs
@@ -33,6 +33,23 @@ namespace ricaun.Revit.DB.Tests
         }
 
         [Test]
+        public void SelectLevelType_Filter()
+        {
+            var first = document.GetFirstElementType<LevelType>();
+            Assert.IsNotNull(first, "First LevelType should not be null.");
+
+            // 'Type Name' - SYMBOL_NAME_PARAM is only available for ElementType
+            var firstByName = document.GetFirstElementType(BuiltInParameter.SYMBOL_NAME_PARAM.Filter(first.Name));
+            Assert.AreEqual(first.Id, firstByName.Id, "First LevelType by name should match the first element type ID.");
+
+            var firstByNameLower = document.GetFirstElementType(BuiltInParameter.SYMBOL_NAME_PARAM.Filter(first.Name.ToLower()));
+            Assert.AreEqual(first.Id, firstByNameLower.Id, "First LevelType by name should match the first element type ID.");
+
+            var firstByNameUpper = document.GetFirstElementType(BuiltInParameter.SYMBOL_NAME_PARAM.Filter(first.Name.ToUpper()));
+            Assert.AreEqual(first.Id, firstByNameUpper.Id, "First LevelType by name should match the first element type ID.");
+        }
+
+        [Test]
         public void SelectLevel()
         {
             var first = document.GetFirstElement<Level>();
@@ -55,6 +72,23 @@ namespace ricaun.Revit.DB.Tests
 
             var elementsCategory = document.GetElements(BuiltInCategory.OST_Levels);
             Assert.AreEqual(1, elementsCategory.Count(), "There should be exactly one Level element in the OST_Levels category.");
+        }
+
+        [Test]
+        public void SelectLevel_Filter()
+        {
+            var first = document.GetFirstElement<Level>();
+            Assert.IsNotNull(first, "First Level should not be null.");
+
+            // 'Name' - DATUM_TEXT is only available for Level
+            var firstByName = document.GetFirstElement(BuiltInParameter.DATUM_TEXT.Filter(first.Name));
+            Assert.AreEqual(first.Id, firstByName.Id, "First Level by name should match the first element ID.");
+
+            var firstByNameLower = document.GetFirstElement(BuiltInParameter.DATUM_TEXT.Filter(first.Name.ToLower()));
+            Assert.AreEqual(first.Id, firstByNameLower.Id, "First Level by name should match the first element ID.");
+
+            var firstByNameUpper = document.GetFirstElement(BuiltInParameter.DATUM_TEXT.Filter(first.Name.ToUpper()));
+            Assert.AreEqual(first.Id, firstByNameUpper.Id, "First Level by name should match the first element ID.");
         }
     }
 }
