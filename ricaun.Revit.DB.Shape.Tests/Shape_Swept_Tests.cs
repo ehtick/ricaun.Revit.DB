4cf98493787d52bc5a4557774be8cd35d98096b1
Luiz Henrique Cassettari <ricaun@gmail.com>
2025-07-29 14:25:16 -0300

    Update `CreateSwept` tests

--- ricaun.Revit.DB.Shape.Tests/Shape_Swept_Tests.cs
+++ ricaun.Revit.DB.Shape.Tests/Shape_Swept_Tests.cs
@@ -11,11 +11,32 @@ namespace ricaun.Revit.DB.Shape.Tests
         {
             var radius = 1.0;
             var center = XYZ.Zero;
-            var solid = ShapeCreator.CreateSwept(Line.CreateBound(center, 2 * radius * XYZ.BasisZ), radius);
+            var line = Line.CreateBound(center, 2 * radius * XYZ.BasisZ);
+            var solid = ShapeCreator.CreateSwept(line, radius);
 
             Assert.IsNotNull(solid, "Solid should not be null");
 
-            Assert.Ignore("This test is ignored, missing implementation.");
+            AssertUtils.Solid(solid, 6, 4);
+        }
+
+        [TestCase(3)]
+        [TestCase(4)]
+        [TestCase(5)]
+        [TestCase(6)]
+        [TestCase(7)]
+        [TestCase(8)]
+        [TestCase(9)]
+        public void CreateSweptSide(int sides)
+        {
+            var radius = 1.0;
+            var center = XYZ.Zero;
+            var line = Line.CreateBound(center, 2 * radius * XYZ.BasisZ);
+            var loop = CurveLoopUtils.CreatePrismLoop(line, radius, sides);
+            var solid = ShapeCreator.CreateSwept(line, loop);
+
+            Assert.IsNotNull(solid, "Solid should not be null");
+
+            AssertUtils.Solid(solid, 3 * sides, 2 + sides);
         }
     }
 }
